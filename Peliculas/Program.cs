9c7e7fdf1269c38e7eca60c842c6edecb29ab526
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-09-12 00:11:17 -0700

    Update Program.cs

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -44,27 +44,45 @@ namespace Peliculas
         {
             return this.Director;
         }
+        //Constructor
+        public Peli(string T, Int16 A)
+        {
+          this.Titulo=T;
+          this.Año=A;
+        }
+        //SobreCarga
+        public Peli()
+        {
+
+        }
+        //Metodo para imprimir
+        public void Imprime()
+        {
+            Console.WriteLine("{0}({1})",this.getTitulo(),this.getAño());
+        }
     }
       class Program
     {
         static void Main(string[] args)
         {
-            Peli P1=new Peli();
-            P1.setTitulo("La forma del agua");
-            P1.setAño(2017);
-            Console.WriteLine("{0}({1})","Pelicula ganadora al oscar: "+ P1.getTitulo(), P1.getAño());
+            Peli P1=new Peli("Pelicula ganadora al oscar: La forma del agua",2017);
+            P1.Imprime(); 
 
+            //P1.setTitulo("La forma del agua");
+            // P1.setAño(2017);
+            //Console.WriteLine("{0}({1})","Pelicula ganadora al oscar: "+ P1.getTitulo(), P1.getAño());
             //P1.Titulo="La forma del agua";
             //P1.Año=2017;
             //P1.Pais="Estados Unidos";
             //P1.Director="Guillermo del toro";
             //Console.WriteLine( "Pelicula ganadora al oscar: "+ P1.Titulo +"\nAño: " +P1.Año );
 
-            Peli P2=new Peli();
-            P2.setTitulo("Roma");
-            P2.setAño(2018);
-            Console.WriteLine("{0}({1})","Pelicula ganadora al oscar: "+ P2.getTitulo(), P2.getAño());
+            Peli P2=new Peli("Pelicula ganadora al oscar: Roma",2018);
+            P2.Imprime();
 
+            //P2.setTitulo("Roma");
+            //P2.setAño(2018);
+            //Console.WriteLine("{0}({1})","Pelicula ganadora al oscar: "+ P2.getTitulo(), P2.getAño());
             //P2.Titulo="Roma";
             //P2.Año=2018;
             //P2.Pais="México";
