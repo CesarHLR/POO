26452b32ded9f30c26df0e756359d6c63f42887c
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-09-11 23:59:22 -0700

    Update Program.cs

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -4,28 +4,72 @@ namespace Peliculas
 {
     class Peli
     {
-        public string Titulo;
-        public Int16 Año;
-        public string Pais;
-        public string Director;      
+        private string Titulo;
+        private Int16 Año;
+        private string Pais;
+        private string Director;      
+        //Metodo Titulo
+        public void setTitulo(string Titulo)
+        {
+           this.Titulo=Titulo;
+        }
+        public string getTitulo()
+        {
+            return this.Titulo;
+        }
+        //Metodo Año
+         public void setAño(Int16 Año)
+        {
+           this.Año=Año;
+        }
+        public Int16 getAño()
+        {
+            return this.Año;
+        }
+        //Metodo Pais
+         public void setPais(string Pais)
+        {
+           this.Pais=Pais;
+        }
+        public string getPais()
+        {
+            return this.Pais;
+        }
+        //Metodo director
+         public void setDirector(string Director)
+        {
+            this.Director=Director;
+        }
+        public string getDirector()
+        {
+            return this.Director;
+        }
     }
       class Program
     {
         static void Main(string[] args)
         {
             Peli P1=new Peli();
-            P1.Titulo="La forma del agua";
-            P1.Año=2017;
-            P1.Pais="Estados Unidos";
-            P1.Director="Guillermo del toro";
-            Console.WriteLine( "Pelicula ganadora al oscar: "+ P1.Titulo +"\nAño: " +P1.Año );
+            P1.setTitulo("La forma del agua");
+            P1.setAño(2017);
+            Console.WriteLine("{0}({1})","Pelicula ganadora al oscar: "+ P1.getTitulo(), P1.getAño());
+
+            //P1.Titulo="La forma del agua";
+            //P1.Año=2017;
+            //P1.Pais="Estados Unidos";
+            //P1.Director="Guillermo del toro";
+            //Console.WriteLine( "Pelicula ganadora al oscar: "+ P1.Titulo +"\nAño: " +P1.Año );
 
             Peli P2=new Peli();
-            P2.Titulo="Roma";
-            P2.Año=2018;
-            P2.Pais="México";
-            P2.Director="Alfonso cuarón";
-            Console.WriteLine( "Pelicula ganadora al oscar: "+ P2.Titulo +"\nAño: " +P2.Año );
+            P2.setTitulo("Roma");
+            P2.setAño(2018);
+            Console.WriteLine("{0}({1})","Pelicula ganadora al oscar: "+ P2.getTitulo(), P2.getAño());
+
+            //P2.Titulo="Roma";
+            //P2.Año=2018;
+            //P2.Pais="México";
+            //P2.Director="Alfonso cuarón";
+            //Console.WriteLine( "Pelicula ganadora al oscar: "+ P2.Titulo +"\nAño: " +P2.Año );
 
 
 
