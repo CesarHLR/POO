b48d8630439c6225ef980f7f18687de24c0cedde
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-09-15 01:10:02 -0700

    Lista peliculas

---
+++ ListaPeliculas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaPeliculas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<String> lista=new List<string>();
+           lista.Add("Pelicula 1: Ex Machina");
+           lista.Add("Pelicula 2: American History X");
+           lista.Add("Pelicula 3: La forma del agua");
+           lista.Add("Pelicula 4: Roma");
+           lista.Add("Pelicula 5: Spirit");
+
+           foreach(string Peliculas in lista)
+           Console.WriteLine(Peliculas);
+        }
+    }
+}
