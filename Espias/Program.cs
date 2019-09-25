4e699a1d572dd75698042c7f085bfecb9aca6356
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-09-24 19:56:53 -0700

    Espias

---
+++ Espias/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Espias
+{
+    class Persona
+    {
+        public string nombre;
+        public string apellido;
+        public Persona()
+        {
+            nombre="Fulano";
+            apellido="De tal";
+
+        }
+    }
+    class Anonymous
+    {
+        public  static void anonimiza(Persona p)
+        {
+            p.nombre="xxxxxx";
+            p.apellido="xxxxx";
+
+        }
+        public static void cambia(ref Persona p)
+        {
+            p=new Persona();
+            p.nombre="new";
+        }
+    }
+    
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        Persona P=new Persona();
+        Console.WriteLine(P.nombre);
+        Anonymous.anonimiza(P);
+        Anonymous.cambia(ref P);
+        Console.WriteLine(P.nombre);
+
+
+        }
+    }
+}
