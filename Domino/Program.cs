2308fce979c51e0aa6c76e2c038df592c2a2e65d
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-10-08 19:57:30 -0700

    domino

---
+++ Domino/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Domino
+{
+    class Domino
+    {
+        public  int A;
+        public  int B;
+        
+        public Domino(int a,int b)
+        {
+         A=a;
+         B=b;
+        }
+        public static int operator +(Domino d1, Domino d2)
+        {
+        return  (d1.A+d2.A+d1.B+d2.B);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Domino a=new Domino (2,0);
+            Domino b=new Domino(4,1);
+            Console.WriteLine(a+b);
+        }
+    }
+}
