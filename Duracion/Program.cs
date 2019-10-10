6e79892b3f2664226b42b13ad55b4a882ae533c8
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-10-09 17:32:01 -0700

    version

---
+++ Duracion/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Duracion
+{
+    class Duracion
+    {
+       public  int Horas;
+       public int Minutos;
+       public int Segundos;
+
+       
+        
+        //Constructor
+        public Duracion(int H, int M, int S )
+        {
+            this.Horas=H;
+            this.Segundos=S;
+            this.Minutos=M;
+            
+            
+        }
+       
+       //Metodo para imprimir
+       public void imprimeHMS()
+       {
+        Console.WriteLine("Horas: {0} Minutos: {1} Segundos: {2}",Horas,Minutos, Segundos);
+       }
+       public int Seg(int H,int M, int S,int ST)
+       { 
+         H=H*3600;
+         M=M*60;
+         ST=H+S+M;
+
+       }
+       public void imprimeS()
+       {
+            Console.WriteLine("Segundos: {0}", ST);
+       }
+       
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear objeto de la clase
+           Duracion hms=new Duracion(8,30,50);
+           //hms.imprimeHMS();
+
+           hms.Seg();
+        }
+    }
+}
