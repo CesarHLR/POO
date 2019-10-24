db6f9a809c999a677e776b2314739079d168b977
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-10-23 17:47:10 -0700

    faceclon terminado

--- Faceclon/Program.cs
+++ Faceclon/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Faceclon
 {
@@ -29,7 +30,8 @@ namespace Faceclon
         }
         public override void PublicaEstado()
         {
-          Console.WriteLine("Usuario: {0} \n fecha: {1} Hora: {2} \n {3} \n Reacciones: {4} Comentarios: {5}",Usuario,Fecha,Hora,Texto,Reacciones,Comentarios);
+          Console.WriteLine(" {0} \n Publicado el {1} a las {2} \n {3} \n {4} Reacciones {5} Comentarios",Usuario,Fecha,Hora,Texto,Reacciones,Comentarios);
+          Console.WriteLine("---------------------------------------------------------------------");
         }
     }
     class Foto:Publicacion
@@ -46,18 +48,41 @@ namespace Faceclon
         }
         public override void PublicaEstado()
         {
-            Console.WriteLine("Usuarios: {0} \n Fecha: {1}  Hora: {2} \n {3} con: {4} \n {5} \n Reacciones: {6} Comentarios: {7}",Usuario,Fecha,Hora,Texto,Etiquetados,foto,Reacciones,Comentarios);
+            Console.WriteLine(" {0} \n Publicado el {1} a las {2} \n {3} (Se encuentra con {4}) \n {5} \n {6} Reacciones {7} Comentarios",Usuario,Fecha,Hora,Texto,Etiquetados,foto,Reacciones,Comentarios);
+            Console.WriteLine("---------------------------------------------------------------------");
+        }
+    }
+    class Link:Publicacion
+    {
+        private string Texto;
+        private string celda;
+        public Link(string u,string h,string f,int r, int c,string t,string ce):base(u,h,f,r,c)
+        {
+            Texto=t;
+            celda=ce;
+        }
+        public override void PublicaEstado()
+        {
+            Console.WriteLine(" {0} \n Publicado el {1} a las {2} \n {3} \n {4} \n {5} Reacciones {6} Comentarios",Usuario,Fecha,Hora,Texto,celda,Reacciones,Comentarios);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-         Estado Pub= new Estado("Cesar","12:31 PM","23 oct 2019",10,2,"Estoy en clase de POO");
-         Pub.PublicaEstado();
+         Estado Pub= new Estado("Cesar Lopez","12:31 PM","23 de octubre 2019",10,2,"Estoy en clase de POO");
+         Foto f=new Foto("Luis Rodriguez","12:48 PM","23 de octubre 2019",90,30,"Aqui en clase de POO","clase.jpg","Enrique");
+         Link Celda=new Link("Esteban Armenta","12:50 PM","23 de octubre 2019",150,20,"Les comparto mi nueva cancion","https://www.youtube.com/watch?v=Z7OZ85Oiul8");
 
-         Foto f=new Foto("Cesar","12:48 PM","23 oct 2019",90,30,"Aqui en clase de POO","clase.jpg","Enrique");
-         f.PublicaEstado();
+         List<Publicacion> pub=new List<Publicacion>();
+         pub.Add(Pub);
+         pub.Add(f);
+         pub.Add(Celda);
+
+         foreach(Publicacion P in pub)
+         {
+             P.PublicaEstado();
+         }
         }
     }
 }
