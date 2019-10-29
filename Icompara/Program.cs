f9a54af165767cdc75c22efd23712ec24f5e2cf3
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-10-29 11:40:29 -0800

    1:3

---
+++ Icompara/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Icompara
+{
+    class Persona
+    {
+        int id;
+        string nombre;
+        public Persona(int id,string nombre)
+        {
+            this.id=id;
+            this.nombre=nombre;
+        }
+    /*public override string ToString()
+    {
+        return string.Format("{0},{1}",id,nombre);
+    }*/
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> pers=new List<Persona>();
+            pers.Add(new Persona(2,"Pam"));
+            pers.Add(new Persona(1,"Jim"));
+            pers.Add(new Persona(3,"Mike"));
+            foreach(Persona p in pers)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
