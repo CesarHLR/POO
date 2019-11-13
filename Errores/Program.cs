8931cd21d22aa1d2eab2bc17858509ad6009a6ea
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-11-13 12:49:28 -0800

    Errores

---
+++ Errores/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Errores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int valor;
+           Console.WriteLine("Ingresa tu edad: ");
+           //string captura=Console.ReadLine();
+           //Console.WriteLine(captura);
+           //Console.ReadKey();
+           try
+           {
+              string captura=Console.ReadLine();
+              valor=int.Parse(captura);
+              //console.WriteLine(valor);
+              Console.WriteLine("En diez años tendras {0}",100/valor);
+           }
+           catch(FormatException)
+           {
+              Console.WriteLine("Formato incorrecto");
+           }
+           catch(DivideByZeroException)
+           {
+             Console.WriteLine("No has nacido");
+           }   
+           Console.ReadKey();
+        }
+    }
+}
