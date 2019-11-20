99fa738a91d6a33bd537530a0c333b7ed0412253
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-11-19 17:28:15 -0800

    Update Program.cs

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -2,11 +2,27 @@
 
 namespace Stack
 {
+    public class Stack<T>
+    {
+        readonly int tamaño;
+        int pila=0;
+        T[] items;
+        public Stack():this(100){}
+        public Stack(int tam)
+        {
+            tamaño=tam;
+            items=new T[tamaño];
+        }
+        public void Push(T item)
+        {
+            
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
