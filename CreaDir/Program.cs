84ceba9dae66633099a2d61a92d67b8e66387e7b
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-11-19 18:47:52 -0800

    CreaDir

---
+++ CreaDir/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace CreaDir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Captura el folder a crear");
+            //Console.WriteLine("Captura el folder a borrar");
+            Console.WriteLine("Captura el archivo a ocultar");
+            string archivos=Console.ReadLine();
+
+            /*string directorio=Console.ReadLine();
+           if(Directory.Exists(directorio))
+            {
+                Console.WriteLine("Ya existe");
+            }
+            else
+            {
+                Directory.CreateDirectory(directorio);
+            }
+            Directory.Delete(directorio,true);*/
+            //File.SetAttributes(archivos)
+        }
+    }
+}
