73760d2540f132a8bcc82cd5acc1bd70782fff5f
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-12-09 15:58:16 -0800

    1

--- Proyecto5y6/Program.cs
+++ Proyecto5y6/Program.cs
@@ -1,7 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Proyecto5y6
 {
+    class Producto
+	{
+		public string Codigo;
+		public string Descripcion;
+		public Decimal Precio;
+		public int Departamento;
+		public int Likes;
+		public Producto(string c,string d,decimal p, int dep, int l)
+		{
+			Codigo=c;
+			Descripcion=d;
+			Precio=p;
+			Departamento=dep;
+			Likes=l;
+		}
+	}
     class Program
     {
         static void Main(string[] args)
