d815015d1efef00276fea342a9839f8e5c2f8604
CesarHLR <54384191+CesarHLR@users.noreply.github.com>
2019-12-09 16:11:09 -0800

    Update Program.cs

--- Proyecto5y6/Program.cs
+++ Proyecto5y6/Program.cs
@@ -22,7 +22,26 @@ namespace Proyecto5y6
 	}
     class ProductoDB
 	{
-        
+        public List<Producto> productos = new List<Producto>();
+        public void EscribeProductosTXT(string archivo)
+		{
+			try {
+				//use la clase filestream para abrir el archivo o crearlo en caso que no exista
+				FileStream Fs=new FileStream(archivo,FileMode.OpenOrCreate,FileAccess.Write);
+				//Crea un escritor para el archivo
+				using(StreamWriter txtOut=new StreamWriter(Fs))
+				{
+					foreach(Producto p in productos)
+					{
+						txtOut.WriteLine("{0}▬{1}▬{2}▬{3}▬{4}",p.Codigo,p.Descripcion,p.Precio,p.Departamento,p.Likes);
+					}
+				}
+			} catch(IOException ex) {
+				Console.WriteLine("[ERROR] No se pudo abrir el archivo ("+ex.Message+")");
+			} catch(Exception ex) {
+				Console.WriteLine("[ERROR] Hay error en la escritura ("+ex.Message+")");
+			}
+		}
     }
     class Program
     {
